b635ddc82712d57730ee5ba4fa7d656b0ecd51fc
Hanxbeen <hanxbeen@gmail.com>
2021-09-15 14:35:44 +0900

    setting : '메인화면 링크'

--- beenblog/src/App.js
+++ beenblog/src/App.js
@@ -4,16 +4,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Let's start to develop own blog</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://medium.com/@hanxbeen"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          BeenBlog
         </a>
       </header>
     </div>
