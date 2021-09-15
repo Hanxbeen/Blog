de6c20c4f96f7473a840740796bc91be73550f24
Hanxbeen <hanxbeen@gmail.com>
2021-09-15 16:20:04 +0900

    feat : 'Routes 페이지 분리하여 Login, Main페이지 이동'

--- beenblog/src/Routes.js
+++ beenblog/src/Routes.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import Login from "./pages/login/Login";
 import MainPage from "./pages/main/Main";
 
 function Routes() {
   return (
     <Router>
+      <ul>
+        <li>
+          <Link to="/beenblog">main</Link>
+        </li>
+        <li>
+          <Link to="/login">login</Link>
+        </li>
+      </ul>
       <Switch>
-        <Route exact path="/" component={MainPage} />
+        <Route exact path="/beenblog" component={MainPage} />
+        <Route exact path="/login" component={Login} />
       </Switch>
     </Router>
   );
---
+++ beenblog/src/pages/login/Login.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Login() {
+  const start = "로그인 페이지 입니다";
+  return <div>{start}</div>;
+}
+
+export default Login;
